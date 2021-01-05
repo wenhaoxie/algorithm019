d498e5145df0ae38a247cddaac3a167b7d3e8b17
wenhaoxie <wenhao@umich.edu>
2021-01-05 02:29:13 -0600

    sample solution

--- Week_08/README.md
+++ Week_08/README.md
@@ -1,1 +1,5 @@
 学习笔记
+### 位运算
+#### 32 位 int 第一位是 符号
+### LRU double linked list + hash map
+### sort quick sort nlogn merge sort nlogn heap sort nlogn
---
+++ Week_08/[242]有效的字母异位词.java
@@ -0,0 +1,44 @@
+//给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。 
+//
+// 示例 1: 
+//
+// 输入: s = "anagram", t = "nagaram"
+//输出: true
+// 
+//
+// 示例 2: 
+//
+// 输入: s = "rat", t = "car"
+//输出: false 
+//
+// 说明: 
+//你可以假设字符串只包含小写字母。 
+//
+// 进阶: 
+//如果输入字符串包含 unicode 字符怎么办？你能否调整你的解法来应对这种情况？ 
+// Related Topics 排序 哈希表 
+// 👍 326 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if ( s.length() != t.length()) return false;
+        char[] charS = s.toCharArray();
+        char[] charT = t.toCharArray();
+
+        int[] charMap = new int[26];
+        for (int i = 0; i < charS.length; i++) {
+            charMap[charS[i] - 'a']++;
+            charMap[charT[i] - 'a']--;
+        }
+        for (int i = 0; i< charMap.length; i++) {
+            if (charMap[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
